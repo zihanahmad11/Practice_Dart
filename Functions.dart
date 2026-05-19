51aaa723db6a34a56e58910b30fdefe65d022125
Rusty Laptop <zihanahmed360@gmail.com>
2026-05-19 08:16:13 +0600

    add few functions

--- Functions.dart
+++ Functions.dart
@@ -1,9 +1,23 @@
 void main() {
-  
+  var name = myName();
+  print(name);
+
+  var yName = yourName();
+  print(yName);
+
 }
 
 /*
 <datatype> fnName() {
-
+// Code to execute goes here
 }
 */
+
+(String, int) myName() {
+  return ("Zihan", 23);
+}
+
+
+String? yourName() {
+  return null;
+}
