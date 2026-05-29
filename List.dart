d8b8f4370b1dd3faa928874b764a7b947d802aec
Rusty Laptop <zihanahmed360@gmail.com>
2026-05-30 00:46:32 +0600

    update list.dart

--- List.dart
+++ List.dart
@@ -1,14 +1,44 @@
 void main(){
   List<Student> studentList = [
- Student("Nuhel"),
- Student("Noman"),
- Student("Tanvir"),
+ Student("Nuhel",  12),
+ Student("Noman",  25),
+ Student("Tanvir", 30),
+ Student("Zihan",  45)
   ];
-  print(studentList[2].name);
-final student = Student('Zihan');
-print(student.name);
+  print(studentList);
+
+List<Student> filteredStudentList = [];
+
+// for(int i = 0; i<studentList.length; i++) {
+//   if(studentList[i].marks >=25 ){
+//   filteredStudentList.add(studentList[i]);
+//   }
+// }
+
+//           ||
+
+for(final student in studentList) {
+  if(student.marks >=25 ){
+  filteredStudentList.add(student);
+  }
 }
+
+  print(filteredStudentList);
+
+
+
+// final student = Student('Zihan', 45);
+// print(student.marks);
+}
+
+
 class Student {
   final String name;
-  Student(this.name);
+  final int marks;
+
+  Student(this.name, this.marks);
+
+  @override
+  String toString() {
+    return name;}
 }
