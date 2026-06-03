82dee4c443268631a217f9090fa63cdc271fe513
zihanahmad11 <zihanahmed360@gmail.com>
2026-06-04 02:29:49 +0600

    add enums

---
+++ Enums.dart
@@ -0,0 +1,16 @@
+void main(){
+  //Enums
+final employee1 = Employee("Zihan", "SWE");
+final employee2 = Employee("Nuhel", "Finance");
+
+final employee3 = Employee("Zihan", "SWE");
+
+
+}
+
+class Employee {
+ final String name;
+ final String type;
+
+ Employee(this.name, this.type);
+}
