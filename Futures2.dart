61499caad62adf3a7597066d9b5b63b44651f848
Rusty Laptop <zihanahmed360@gmail.com>
2026-06-08 00:46:03 +0600

    update future2.dart

--- Futures2.dart
+++ Futures2.dart
@@ -1,8 +1,11 @@
 
-// import 'package:http/http.dart' as http;
+import 'package:http/http.dart' as http;
 
-void main() {
-  //  http.get();
+void main() async {
+  var url = Uri.https('jsonplaceholder.typicode.com', 'users');
+   final res = await http.get(url);
+
+   print(res.body);
 
   
 }
