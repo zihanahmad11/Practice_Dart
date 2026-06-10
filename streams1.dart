4af25eed91c0d3a318b96f864900a791f0da5bd8
Rusty PC <zihanahmad360@gmail.com>
2026-06-10 12:15:16 +0600

    add comment for pull request and fork system

--- streams1.dart
+++ streams1.dart
@@ -1,17 +1,21 @@
- void main() async {
+void main() async {
   // streams
-counDown().listen((val) {
-  print(val);
-}, onDone: () {
-print("Hey it's completed fine");
-});
+  counDown().listen(
+    (val) {
+      print(val);
+    },
+    onDone: () {
+      print("Hey it's completed fine");
+    },
+  );
 
-print('Hi there');
- }
-
- Stream <int> counDown() {
-return Stream.periodic(Duration(seconds: 1), (val) {
-  return val;
-});
+  print('Hi there');
 }
+
+Stream<int> counDown() {
+  return Stream.periodic(Duration(seconds: 1), (val) {
+    return val;
+  });
+}
+// this commit is for testing the fork system and pull request system of github. I am learning how to use it.
  
