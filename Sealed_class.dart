76e810b9ef7e0237b3a9b44d22b57e066274cb00
yusufAhmad11 <zihanahmed360@icloud.com>
2026-06-20 12:30:56 +0600

    update a comment (#11)
    Co-authored-by: Rusty PC <zihanahmad360@gmail.com>

--- Sealed_class.dart
+++ Sealed_class.dart
@@ -1,17 +1,20 @@
-void main(){
-Animal animal= Cat();
-switch (animal) {
-  case Dog():
-  print('dog');
-  case Cat():
-  print('cat');
-  case Human():
-  print('human');
-}
+void main() {
+  Animal animal = Cat();
+  switch (animal) {
+    case Dog():
+      print('dog');
+    case Cat():
+      print('cat');
+    case Human():
+      print('human');
+  }
 }
 
 sealed class Animal {}
-class Human implements Animal{}
-class Dog implements Animal{}
+
+class Human implements Animal {}
+
+class Dog implements Animal {}
+
 class Cat extends Animal {}
-// ok
+// ok.
